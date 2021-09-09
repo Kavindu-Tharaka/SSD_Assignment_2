6a31851a410fd9ec4dd1fd61ce3db9ef82ad228c
Tharaka W.C.M.K <it18001112@my.sliit.lk>
2021-09-09 20:26:02 +0530

    Add basic routings

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+import SigninPage from './Pages/SigninPage'
+import CallbackPage from './Pages/CallbackPage'
+import PageNotFoundPage from './Pages/PageNotFoundPage'
+
+function App() {
+  return (
+    <div className="App">
+
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={SigninPage} />
+        <Route path="/facebookapp/callback" exact component={CallbackPage} />
+        <Route path="*" exact component={PageNotFoundPage} />
+      </Switch>
+    </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
