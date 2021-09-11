96c5b73065c49504492a637dc56af18dcc515a29
Perera H.D.D.S <it18006544@my.sliit.lk>
2021-09-11 18:33:40 +0530

    added oject url and implemented backedn

--- src/Pages/DriveUploadPage.jsx
+++ src/Pages/DriveUploadPage.jsx
@@ -1,39 +1,73 @@
-import React,{useState} from "react";
-import path from 'path'
+import React, { useState } from "react";
+import path from "path";
+// import { google } from "googleapis";
+import axios from 'axios'
 
 function DriveUploadPage() {
-    const [imageDetails,setImageDetails] = useState()
+  const [imageDetails, setImageDetails] = useState();
 
-    const onFileChange = (e) => {
-        e.preventDefault()
-       console.log(e.target.files[0].name)
-       const image_base_64 = URL.createObjectURL(e.target.files[0])
-       const image_file = e.target.files[0]
 
-       setImageDetails({
-         image:image_base_64,
-         file:image_file
-       })
+  const onFileChange = async (e) => {
+    e.preventDefault();
+    const image = e.target.files[0]
+    // const base64 = await convertToBase64(image)
+    // console.log(base64)
+    // console.log(e.target.files[0].name);
+    const image_base_64 = URL.createObjectURL(e.target.files[0]);
+    const image_file = e.target.files[0];
+
+    setImageDetails({
+      image: image_base_64,
+      file: image_file,
+    });
+  };
+
+
+  // const convertToBase64 = file => {
+  //   return new Promise((resolve,reject) => {
+  //     const filereader = new FileReader();
+  //     filereader.readAsDataURL(file);
+
+  //     filereader.onload = () => {
+  //       resolve(filereader.result)
+  //     }
+
+  //     filereader.onerror = error => {
+  //       reject(error)
+  //     }
+  //   })
+  // }
+
+
+  const onSubmit = async () => {
+    try {
+      console.log('shdbaj')
+      const res =  await axios.post("/upload",{
+        data:imageDetails
+      })
+    } catch (error) {
+      
     }
-
-
-    const onSubmit = () => {
-      console.log(imageDetails)
-      const formdata =new FormData()
-      formdata.append('data', imageDetails.image)
-      formdata.append('file_details', imageDetails.file)
-    }
-
+    // console.log(imageDetails);
+    // const formdata = new FormData();
+    // formdata.append("data", imageDetails.image);
+    // formdata.append("file_details", imageDetails.file);
+    // uploadtoDrive();
+  };
 
   return (
     <>
-    {/* <form onSubmit={onSubmitHandler}> */}
-      <input type="file" onChange={onFileChange}/>
-      <button className="btn btn-primary btn-lg mt-5" type="submit" onClick={onSubmit}>
+      {/* <form onSubmit={onSubmitHandler}> */}
+      <input type="file" onChange={onFileChange} />
+      <button
+        className="btn btn-primary btn-lg mt-5"
+        type="submit"
+        onClick={onSubmit}
+      >
         Upload Images to Drive
       </button>
-      
-    {/* </form> */}
+
+      {/* </form> */}
     </>
   );
 }
