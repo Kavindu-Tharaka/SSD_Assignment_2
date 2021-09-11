995dd7d259f3cc799da0c974809948b579306486
Dilanka R.M.T <it18009132@my.sliit.lk>
2021-09-11 15:00:46 +0530

    Design 404 page

--- src/Pages/PageNotFoundPage.jsx
+++ src/Pages/PageNotFoundPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import pageNotFoundImage from '../Images/notfound.gif'
 
 function PageNotFoundPage() {
-    return (
-        <div>
-            Page Not Found!
-        </div>
-    )
+	return (
+		<div id='notfound'>
+            <img src={pageNotFoundImage} alt="not found" />
+			<div class='notfound'>
+				<div class='notfound-404'></div>
+				<h2>Oops! Page Not Be Found</h2>
+				<p>
+					Sorry but the page you are looking for does not exist, have
+					been removed. name changed or is temporarily unavailable
+				</p>
+				<a href='/'>Back to homepage</a>
+			</div>
+		</div>
+	);
 }
 
-export default PageNotFoundPage
+export default PageNotFoundPage;
