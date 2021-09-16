5abb19c9d5789e372ad960f8f830314daa831b6a
Dilanka R.M.T <it18009132@my.sliit.lk>
2021-09-16 20:31:02 +0530

    Bug fixes and refact

--- server/index.js
+++ server/index.js
@@ -1,57 +1,53 @@
-const express = require("express");
-const { google } = require("googleapis");
-const { OAuth2Client } = require("google-auth-library");
-const fs = require("fs");
-const got = require("got");
+const express = require('express');
+const { google } = require('googleapis');
+const { OAuth2Client } = require('google-auth-library');
 const app = express();
 app.use(express.json());
 
-
 //Initializing client id,secret and redirect URI constants
 const CLIENT_ID =
-  "132620122654-ij2jqq591tmp3726366j4veh92806vkv.apps.googleusercontent.com";
-const CLIENT_SECRET = "_WHhVdjkglEZEfOcAGx6OphH";
-const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+	'132620122654-ij2jqq591tmp3726366j4veh92806vkv.apps.googleusercontent.com';
+const CLIENT_SECRET = '_WHhVdjkglEZEfOcAGx6OphH';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
 const REFRESH_TOKEN =
-  "1//04L2z0DhgyeYzCgYIARAAGAQSNwF-L9IrfQMCgAOhJzu3h3ftSvGcLmACmAzLjKlTpuXNy-Z8juJ1o6l8iKvsFGV5EA-wtOMNtYU";
+	'1//04L2z0DhgyeYzCgYIARAAGAQSNwF-L9IrfQMCgAOhJzu3h3ftSvGcLmACmAzLjKlTpuXNy-Z8juJ1o6l8iKvsFGV5EA-wtOMNtYU';
 
 // creating OAuthtClient object
 const oAuthtoClient = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
 //creating drive object
 const drive = google.drive({
-  version: "v3",
-  auth: oAuthtoClient,
+	version: 'v3',
+	auth: oAuthtoClient,
 });
 
 //setting OAuth2Client credentials
 oAuthtoClient.setCredentials({
-  refresh_token: REFRESH_TOKEN,
+	refresh_token: REFRESH_TOKEN,
 });
 
+/**
+ * End point to upload images to the google drive request body will contain the url of the image
+ * if success 200 status code will be sent as the response to the client if there is an error status
+ * code of 400 will be sent to the client side
+ */
+app.post('/upload', async (req, res) => {
+	try {
+		const res = await drive.files.create({
+			requestBody: {
+				name: req.body.data.file.name,
+				mimeType: 'image/jpg',
+			},
+			media: {
+				mimeType: 'image/jpg',
+				body: req.body.data.file.url,
+			},
+		});
 
-//end point to upload images to the google drive
-//request body will contain the url of the image
-//if success 200 status code will be sent as the response to the client
-//if there is an error status code of 400 will be sent to the client side
-app.post("/upload", async (req, res) => {
-  try {
-    const res = await drive.files.create({
-      requestBody: {
-        name: req.body.data.file.name,
-        mimeType: "image/jpg",
-      },
-      media: {
-        mimeType: "image/jpg",
-        body: req.body.data.file.url,
-      },
-    });
-    UploadPage;
-
-    console.log(res);
-  } catch (error) {
-    console.log(error);
-  }
+		console.log(res);
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 const PORT = 5000;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
 
-import SigninPage from './Pages/SigninPage'
-import CallbackPage from './Pages/CallbackPage'
-import PageNotFoundPage from './Pages/PageNotFoundPage'
-import UploadPage from './Pages/UploadPage'
+import SigninPage from './Pages/SigninPage';
+import CallbackPage from './Pages/CallbackPage';
+import PageNotFoundPage from './Pages/PageNotFoundPage';
 
 function App() {
-  return (
-    <div className="App">
-
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={SigninPage} />
-        <Route path="/facebookapp/callback" exact component={CallbackPage} />
-        <Route path="/uploadfiles" exact component={UploadPage} />
-        <Route path="*" exact component={PageNotFoundPage} />
-      </Switch>
-    </BrowserRouter>
-
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Switch>
+					<Route path='/' exact component={SigninPage} />
+					<Route
+						path='/facebookapp/callback'
+						exact
+						component={CallbackPage}
+					/>
+					<Route path='*' exact component={PageNotFoundPage} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
